bc474b90939d6f5f209c42d11e22d5ac3dc188db
Yingjian Pei <yjpei@ucsd.edu>
2021-04-05 21:40:31 -0700

    test failed build

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default functxion App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
