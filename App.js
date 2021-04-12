cfea92693bfae5703d0382bc04e164f9df980735
Yingjian Pei <yjpei@ucsd.edu>
2021-04-12 14:03:40 -0700

    set page to dashboard

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Dashboard">
         <Stack.Screen
           name="Dashboard"
           component={Dashboard}
