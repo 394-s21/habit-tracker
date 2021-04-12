83b2627e485e691665172df49974c4bfca4b04d4
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-04-12 10:27:35 -0500

    Added state control for button

--- components/CommonCompCompleteHabit.js
+++ components/CommonCompCompleteHabit.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 
-const CompleteHabit = ({completed, select}) => (
-    <TouchableOpacity onPress={() => {select(completed);}}>
-      <Text style={styles.courseText}>
-        {completed ? 'Habit Completed!' : 'Completed Habit?'}
-      </Text>
-    </TouchableOpacity>
-  );
+const CompleteHabit = ({ completed, select }) => {
+    //TODO: Change this to updating database
+    const [completed, setCompleted] = useState(false);
+    const select = course => setCompleted(!completed);
+
+    return (
+        <TouchableOpacity onPress={() => { select(completed); }}>
+            <Text style={styles.courseText}>
+                {completed ? 'Habit Completed!' : 'Completed Habit?'}
+            </Text>
+        </TouchableOpacity>
+    );
+};
 
 
 const styles = StyleSheet.create({
@@ -25,11 +31,11 @@ const styles = StyleSheet.create({
         backgroundColor: '#e5e5e5',
         borderColor: '#fff',
     },
-    buttonText:{
-      color: '#fff',
-      fontSize: 18,
-      textAlign: 'center',
+    buttonText: {
+        color: '#fff',
+        fontSize: 18,
+        textAlign: 'center',
     },
-  });
+});
 
-  export default CompleteHabit;
+export default CompleteHabit;
