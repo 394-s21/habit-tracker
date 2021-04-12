f08649ef219db48a0256a7ebd1e29a2d1bf6dee6
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-04-12 10:15:22 -0500

    Initial habit complete button, untested

---
+++ components/CommonCompCompleteHabit.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+
+const CompleteHabit = ({completed, select}) => (
+    <TouchableOpacity onPress={() => {select(completed);}}>
+      <Text style={styles.courseText}>
+        {completed ? 'Habit Completed!' : 'Completed Habit?'}
+      </Text>
+    </TouchableOpacity>
+  );
+
+
+const styles = StyleSheet.create({
+    habitButton: {
+        flex: 1,
+        borderRadius: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        height: 60,
+        padding: 10,
+        minWidth: 90,
+        maxWidth: 90,
+        backgroundColor: '#e5e5e5',
+        borderColor: '#fff',
+    },
+    buttonText:{
+      color: '#fff',
+      fontSize: 18,
+      textAlign: 'center',
+    },
+  });
+
+  export default CompleteHabit;
