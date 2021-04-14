d8c884c92b03fcd983a9722202adff4ac17baa2c
Yingjian Pei <yjpei@ucsd.edu>
2021-04-13 20:59:40 -0700

    worked for both ios and android

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import Login from './screens/Login';
+import Dashboard from './screens/Dashboard';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import DashboardStack from './navigations/dashboardStack';
@@ -10,7 +11,7 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Dashboard">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen
           name="Dashboard"
           component={DashboardStack}
--- screens/Login.js
+++ screens/Login.js
@@ -10,67 +10,43 @@ if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig)
 }
 class Login extends Component {
-  isUserEqual = (googleUser, firebaseUser) => {
-    if (firebaseUser) {
-      var providerData = firebaseUser.providerData;
-      for (var i = 0; i < providerData.length; i++) {
-        if (
-          providerData[i].providerId ===
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID 
-        ) {
-          // We don't need to reauth the Firebase connection.
-          return true;
-        }
-      }
-    }
-    return false;
-  };
   onSignIn = googleUser => {
-    console.log('Google Auth Response', googleUser);
     // We need to register an Observer on Firebase Auth to make sure auth is initialized.
     var unsubscribe = firebase.auth().onAuthStateChanged(
-      function(firebaseUser) {
+      function() {
         unsubscribe();
         // Check if we are already signed-in Firebase with the correct user.
-        if (!this.isUserEqual(googleUser, firebaseUser)) {
           // Build Firebase credential with the Google ID token.
-          var credential = firebase.auth.GoogleAuthProvider.credential(
-            googleUser.idToken,
-            googleUser.accessToken
-          );
-          // Sign in with credential from the Google user.
-          firebase
-            .auth()
-            .signInWithCredential(credential)
-            .then(function(result) {
-              console.log('user signed in ');
-              if (result.additionalUserInfo.isNewUser) {
-                firebase
-                  .database()
-                  .ref('/users/' + result.user.uid)
-                  .set({
-                    gmail: result.user.email,
-                    profile_picture: result.additionalUserInfo.profile.picture,
-                    first_name: result.additionalUserInfo.profile.given_name,
-                    last_name: result.additionalUserInfo.profile.family_name,
-                    created_at: Date.now()
-                  })
-                  .then(function(snapshot) {
-                    // console.log('Snapshot', snapshot);
-                  });
-              } else {
-                firebase
-                  .database()
-                  .ref('/users/' + result.user.uid)
-                  .update({
-                    last_logged_in: Date.now()
-                  });
-              }
-            });
-        } else {
-          console.log('User already signed-in Firebase.');
-        }
-      }.bind(this)
+        var credential = firebase.auth.GoogleAuthProvider.credential(
+          googleUser.idToken,
+          googleUser.accessToken
+        );
+        // Sign in with credential from the Google user.
+        firebase
+          .auth()
+          .signInWithCredential(credential)
+          .then(function(result) {
+            console.log('user signed in ');
+            firebase
+              .database()
+              .ref('/users/' + result.user.uid)
+              .set({
+                gmail: result.user.email,
+                profile_picture: result.additionalUserInfo.profile.picture,
+                first_name: result.additionalUserInfo.profile.given_name,
+                last_name: result.additionalUserInfo.profile.family_name,
+                created_at: Date.now()
+              })
+          }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            console.log("user not authenticated")
+          })}.bind(this)
     );
   };
 
@@ -83,7 +59,6 @@ class Login extends Component {
         androidClientId: "547778785940-c4auvl9ajnaba4omhbgrqgkp7aaun0uh.apps.googleusercontent.com",
         scopes: ['profile', 'email']
       });
-
       if (result.type === 'success') {
         this.onSignIn(result);
         console.log("successful sign in")
