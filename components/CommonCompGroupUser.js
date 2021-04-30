c8e28a9e908dede08f665e9b0c610ffd8bfbb5fa
jakerogers2022 <jakerogers2022@u.northwestern.edu>
2021-04-30 10:17:17 -0500

    fix scroll issue with names

--- components/CommonCompGroupUser.js
+++ components/CommonCompGroupUser.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
 const CommonCompGroupUser = ({memberUserName}) => (
-    <View style={styles.userButton}>
+    <TouchableOpacity style={styles.userButton}>
       <Text style={styles.userText}>
         {memberUserName}
       </Text>
-    </View>
+    </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
